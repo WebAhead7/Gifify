10a30e57f37b00c9a7c42749dde64f542aa50100
nizarhalloun <nshalloun@hotmail.com>
2020-10-22 12:16:17 +0300

    fixed code duplication

--- index.js
+++ index.js
@@ -1,59 +1,39 @@
-var homeDiv = document.getElementById('home');
-var aboutDiv = document.getElementById('about');
-var contactDiv = document.getElementById('contact');
-// intial display
-homeDiv.style.display = 'block';
-contactDiv.style.display = 'none';
-aboutDiv.style.display = 'none';
+const homeElement = document.getElementById('home');
+const aboutElement = document.getElementById('about');
+const contactElement = document.getElementById('contact');
+
+function showMenueDivs(home, about, contact) {
+  homeElement.style.display = home ? 'block' : 'none';
+  aboutElement.style.display = about ? 'block' : 'none';
+  contactElement.style.display = contact ? 'block' : 'none';
+}
+
+// Show only home
+showMenueDivs(true, false, false);
 
 // about us button function
 function aboutFunction() {
-  if (homeDiv.style.display === 'block') {
-    homeDiv.style.display = 'none';
-    contactDiv.style.display = 'none';
-    aboutDiv.style.display = 'block';
-  } else {
-    homeDiv.style.display = 'none';
-    contactDiv.style.display = 'none';
-    aboutDiv.style.display = 'block';
+  if (
+    homeElement.style.display === 'block' ||
+    contactElement.style.display === 'block'
+  ) {
+    showMenueDivs(false, true, false);
   }
 }
 // contact us button function
 function contactFunction() {
-  if (homeDiv.style.display === 'block') {
-    homeDiv.style.display = 'none';
-    contactDiv.style.display = 'block';
-    aboutDiv.style.display = 'none';
-  } else {
-    homeDiv.style.display = 'none';
-    aboutDiv.style.display = 'none';
-    contactDiv.style.display = 'block';
+  if (
+    homeElement.style.display === 'block' ||
+    aboutElement.style.display === 'block'
+  ) {
+    showMenueDivs(false, false, true);
   }
 }
-// return button about to home
-function aboutReturn() {
-  if (aboutDiv.style.display === 'none') {
-    aboutDiv.style.display = 'block';
-    homeDiv.style.display = 'none';
-    contactDiv.style.display = 'none';
-  } else {
-    aboutDiv.style.display = 'none';
-    contactDiv.style.display = 'none';
-    homeDiv.style.display = 'block';
-  }
+// return to home
+function returnToMain() {
+  showMenueDivs(true, false, false);
 }
-// return button contact to home
-function contactReturn() {
-  if (contactDiv.style.display === 'none') {
-    aboutDiv.style.display = 'none';
-    homeDiv.style.display = 'none';
-    contactDiv.style.display = 'block';
-  } else {
-    aboutDiv.style.display = 'none';
-    contactDiv.style.display = 'none';
-    homeDiv.style.display = 'block';
-  }
-}
+
 /////////////////////
 
 const idKey = 'OiRmKWWUtNcSOtfRmlVDgYzuRKHSZPsz';
@@ -69,15 +49,16 @@ function showSpinner(show) {
   spinner.style.display = show ? 'block' : 'none';
 }
 
+// Invoked when the user press submit
 function submitbutton() {
-  const inputText = document.getElementById('searchBox').value;
+  const searchBoxText = document.getElementById('searchBox').value;
 
   fetch(
     'https://api.giphy.com/v1/gifs/search' +
       '?api_key=' +
       idKey +
       '&q=' +
-      inputText
+      searchBoxText
   )
     .then((data) => data.json())
     .then((json) => {
@@ -85,11 +66,12 @@ function submitbutton() {
       const gif = document.getElementsByClassName('gif');
       gif[0].src = embed_url;
 
-      //const lyrics = getLyrics(inputText); //! problem
+      //const lyrics = getLyrics(searchBoxText); //! problem
 
       var image = new Image();
       image.src = embed_url;
 
+      // Show the spinner and turn it off when the image loads
       var interval = setInterval(() => {
         var isComplete = image.complete;
         if (isComplete) {
